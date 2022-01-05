3f0f59dcb56e5bb119248ba7269b9af85624e98d
hominjang <51106030+hominjang@users.noreply.github.com>
2022-01-05 20:07:03 +0900

    22/01/05 BaekJoon queue solved
    9012 큐 문제 풀어봄

--- codingTest/venv/BaekJoon/2667.py
+++ codingTest/venv/BaekJoon/2667.py
@@ -1,7 +1,42 @@
 x = int(input())
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 arr = []
-for i in range(x):
-    arr.append(list(map(int, input().split(""))))
+houseCount = []
 
-print (arr)
+count = 0
+
+for i in range(x):
+    arr.append(list(map(int, input())))
+
+
+def fun(i, j):
+    global count
+    # 범위 밖을 벗어날 때
+    if i < 0 or j < 0 or i >= x or j >= x:
+        return False
+
+    if arr[i][j] == 0:
+        return False
+
+    if arr[i][j] == 1:
+        arr[i][j] = 0
+        count = count + 1
+        for k in range(4):
+            mx = dx[k] + i
+            my = dy[k] + j
+            fun(mx, my)
+        return True
+    return False
+
+
+for i in range(x):
+    for j in range(x):
+        if fun(i, j):
+            houseCount.append(count)
+            count = 0
+
+print(len(houseCount))
+for i in houseCount:
+    print(i)
--- codingTest/venv/BaekJoon/9012.py
+++ codingTest/venv/BaekJoon/9012.py
@@ -2,28 +2,18 @@ x = int(input())
 arr = []
 left = 0
 right = 0
-open_close = 0
+
 for i in range(x):
     arr.append(list(map(str, input())))
 
 for i in arr:
     for j in i:
-        if j == "(":
+        if j is "(":
             left = left + 1
-            open_close = open_close + 1
-
-        if j == ")":
+        if j is ")":
             right = right + 1
-            open_close = open_close - 1
-
-        if open_close < 0:
-            right = 99999
-            break
 
     if right == left:
         print("YES")
     else:
         print("NO")
-    right = 0
-    left = 0
-    open_close = 0
