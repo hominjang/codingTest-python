58421fa8988bd40b9579ba719c61bfbba214b1f0
hominjang <51106030+hominjang@users.noreply.github.com>
2022-01-05 19:06:24 +0900

    22/01/05 BaekJoon solved
    DFS Q solved

--- codingTest/main.py
+++ codingTest/main.py
@@ -4,7 +4,7 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 arr = []
 houseCount = []
-houseCount2 = []
+
 count = 0
 
 for i in range(x):
@@ -13,39 +13,30 @@ for i in range(x):
 
 def fun(i, j):
     global count
-    arr[i][j] = 2
-    count = count + 1
-    for k in range(4):
-        mx = dx[k] + i
-        my = dy[k] + j
+    # 범위 밖을 벗어날 때
+    if i < 0 or j < 0 or i >= x or j >= x:
+        return False
 
-        # 범위 밖을 벗어날 때
-        if mx < 0 or my < 0 or mx >= x or my >= x:
-            continue
+    if arr[i][j] == 0:
+        return False
 
-        if arr[mx][my] == 0:
-            continue
-
-        if arr[mx][my] == 1:
+    if arr[i][j] == 1:
+        arr[i][j] = 0
+        count = count + 1
+        for k in range(4):
+            mx = dx[k] + i
+            my = dy[k] + j
             fun(mx, my)
-            houseCount.append(count)
-            print(mx, end="")
-            print(my)
+        return True
+    return False
 
 
 for i in range(x):
     for j in range(x):
-        if arr[i][j] == 1:
-            fun(i, j)
+        if fun(i, j):
+            houseCount.append(count)
+            count = 0
 
-arrS = len(houseCount)
-houseCount2 = houseCount[:]
-
-for i in range(1, arrS):
-    houseCount2[i] = houseCount[i] - houseCount[i - 1]
-print(houseCount)
-houseCount2.sort()
-
-for i in houseCount2:
-    if i != 0:
-        print(i)
+print(len(houseCount))
+for i in houseCount:
+    print(i)
